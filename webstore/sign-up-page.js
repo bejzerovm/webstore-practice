9bec347bdaa56134320322dba7e0e5ae511e6002
bejzerovm <109041745+bejzerovm@users.noreply.github.com>
2023-01-28 19:04:45 +0000

    saving

--- webstore/sign-up-page.js
+++ webstore/sign-up-page.js
@@ -1,8 +1,24 @@
 //Model
 
 
-const signUp = (email, password)
+
+
+
+
+
+const signUp = () => {
   const password = passwordBox.value
+  const email = emailBox.value
+  if (email.length != 0 && password.length != 0 )  {
+    localStorage.setItem('email', JSON.stringify(email))
+    localStorage.setItem('password', JSON.stringify(password))
+  } else { 
+    passwordBox.placeholder='Enter your password'
+    emailBox.placeholder='Enter your email'
+
+  }
+
+}
 
 //View
 
