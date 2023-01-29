c86906da93a200ade2fd5685a32b935f942db784
bejzerovm <109041745+bejzerovm@users.noreply.github.com>
2023-01-29 16:58:40 +0000

    mass

--- webstore/webstore.js
+++ webstore/webstore.js
@@ -22,7 +22,7 @@ let op = 1
 
 const changeImage = () => {
   if (num < 0) {
-    num = num+3
+    num = num+9
   }
   num = num % 3
   image1.src = images[num]
@@ -33,7 +33,7 @@ const changeImage = () => {
 
 const opacityChange2 = () => {
   if (op < 1) {
-    op = op +0.05
+    op = op +0.0125
     image1.style.opacity = op
   } else {
     clearInterval(nIntervId);
@@ -44,24 +44,24 @@ const opacityChange2 = () => {
 
 
 const opacityChange = () => {
-  if (op <= 0.1) {
+  if (op <= 0.2) {
     changeImage()
     fadeIn()
     
   } else {
-    op = op - 0.05
+    op = op - 0.0125
     image1.style.opacity = op
   }
   
 }
 const fadeOut = () => {
   if (!nIntervId) {
-      nIntervId = setInterval(opacityChange, 25);
+      nIntervId = setInterval(opacityChange, 6.75);
     }
 }
 const fadeIn = () => {
   if (!nIntervId) {
-      nIntervId = setInterval(opacityChange2, 25);
+      nIntervId = setInterval(opacityChange2, 6.75);
     }
   }
 
